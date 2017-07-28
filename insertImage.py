ebc19d57426889c67c5f982c497bb029d6c11843
Fred Kim <yhk1038@hanmail.net>
2017-07-28 22:59:03 +0900

    stopable limit move

--- insertImage.py
+++ insertImage.py
@@ -7,31 +7,66 @@ pygame.display.set_caption('이미지 삽입 테스트')
 # pygame.mixer.music.load('IU-maum.mp3')
 # pygame.mixer.music.play(-1)
 
+# Size
 display_width = 800
 display_height = 600
+
+img_width = 100
+img_height = 100
+
+start_x_position = 0
+start_y_position = display_height - img_height
+
+end_left    = 0
+end_right   = display_width - img_width
+end_top     = 0
+end_bottom  = display_height - img_height
+
+# Setup MetaData
 ourScreen = pygame.display.set_mode((display_width, display_height))
 clock = pygame.time.Clock()
 
 myImg = pygame.image.load('gorae.png')
+myImg = pygame.transform.scale(myImg, (img_width, img_height))
 
+# Image Rendering
 def myimg(x, y):
 	ourScreen.blit(myImg, (x, y))
 	
-x = display_width * 0.5
-y = display_height * 0.5
+def move(pressed, x, y):
+	if pressed[pygame.K_UP]:
+		y -= 10
+		if y < end_top:
+			y = end_top
+	if pressed[pygame.K_DOWN]:
+		y += 10
+		if y > end_bottom:
+			y = end_bottom
+	if pressed[pygame.K_LEFT]:
+		x -= 10
+		if x < end_left:
+			x = end_left
+	if pressed[pygame.K_RIGHT]:
+		x += 10
+		if x > end_right:
+			x = end_right
+	return x, y
+	
 
+x = start_x_position
+y = start_y_position
+
+# Main Loop
 finished = False
 while not finished:
+	# Event Loop
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			finished = True
 	ourScreen.fill((255, 255, 200))
 	
 	pressed = pygame.key.get_pressed()
-	if pressed[pygame.K_UP]: y -= 10
-	if pressed[pygame.K_DOWN]: y += 10
-	if pressed[pygame.K_LEFT]: x -= 10
-	if pressed[pygame.K_RIGHT]: x += 10
+	x, y = move(pressed, x, y)
 	myimg(x, y)
 	
 	pygame.display.flip()
