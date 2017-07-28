9113909df76c257df6e0ce7048e3ddf64cb366a4
Fred Kim <yhk1038@hanmail.net>
2017-07-28 21:55:12 +0900

    move image

--- insertImage.py
+++ insertImage.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pygame
 pygame.init()
 pygame.display.set_caption('이미지 삽입 테스트')
@@ -5,6 +7,7 @@ pygame.display.set_caption('이미지 삽입 테스트')
 display_width = 800
 display_height = 600
 ourScreen = pygame.display.set_mode((display_width, display_height))
+clock = pygame.time.Clock()
 
 myImg = pygame.image.load('gorae.png')
 
@@ -20,9 +23,16 @@ while not finished:
 		if event.type == pygame.QUIT:
 			finished = True
 	ourScreen.fill((255, 255, 200))
+	
+	pressed = pygame.key.get_pressed()
+	if pressed[pygame.K_UP]: y -= 10
+	if pressed[pygame.K_DOWN]: y += 10
+	if pressed[pygame.K_LEFT]: x -= 10
+	if pressed[pygame.K_RIGHT]: x += 10
 	myimg(x, y)
 	
 	pygame.display.flip()
+	clock.tick(60)
 	
 pygame.quit()
 quit()
